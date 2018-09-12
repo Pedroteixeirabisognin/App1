7c76584fcb6e9de0258426b1e1aa5d1e1cf849b9
Pedro Teixeira <pedroteixeirabisognin@gmail.com>
2018-09-12 16:57:21 -0300

    Mais componentes

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 //Indica qual o novo componente
 @Component({
     selector: 'app-topo',
-    templateUrl: './topo.component.html'
+    templateUrl: './topo.component.html',
     //Template serve para não ter que implementar um arquivo teamplate
     //template: '<p>Esse é o componente Topo</p>'
+    //Você pode usar ' para estilos de uma linha e pode usar ` para estilos de mais de uma linha
+    /*styles :[`.exemplo{
+        color:red
+    } `]*/
+    styleUrls: ['./topo.component.css']
 })
 export class TopoComponent{}
